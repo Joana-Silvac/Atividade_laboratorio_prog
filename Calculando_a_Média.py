78343b27c331d1274d32f925f6ba6fe37d6a14d8
Joana Silva <joanadasilva.pr@gmail.com>
2025-08-06 16:59:32 -0300

    solução concluída do Calculando a media

---
+++ Calculando_a_Média.py
@@ -0,0 +1,13 @@
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def media(p,n):
+    valor= p/n
+    print(valor)
+
+def soma(lista):
+    n=len(lista)
+    p=sum(lista)
+    media(p,n)
+
+
+soma(numeros)
