9b6dda324142323300150cf6db5d5ff932552e59
Joana Silva <joanadasilva.pr@gmail.com>
2025-08-06 15:35:15 -0300

    solução concluída da atv: encontrando o meior número

---
+++ Encontrando_o_Maior_Número.py
@@ -0,0 +1,8 @@
+#Escreva uma função que receba uma lista de números e retorne o maior número encontrado nela.
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def maior(numeros):
+    print(max(numeros))
+    return
+maior(numeros)
