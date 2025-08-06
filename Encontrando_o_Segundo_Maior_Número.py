eee6ec6e93313696fff96d4e27d01261a99d8916
Joana Silva <joanadasilva.pr@gmail.com>
2025-08-06 15:41:53 -0300

    solução concluída da atv Encontrando o segundo maior número

---
+++ Encontrando_o_Segundo_Maior_Número.py
@@ -0,0 +1,13 @@
+#Crie uma função que retorne o segundo maior número de uma lista. Considere que a lista pode ter números duplicados.
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+
+def segundo(numeros):
+    x= max(numeros)
+    lista_nova=[i for i in numeros if i!= x]
+    print(max(lista_nova))
+    return 
+
+
+segundo(numeros)
